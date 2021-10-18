a83b9cbb53e9cb27e4c7d4da446346a9bfc9c975
Jorge <jor.saezb@duocuc.cl>
2021-10-18 17:27:48 -0300

    imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, MatProgressSpinnerModule, MatTabsModule, MatButtonModule, MatIconModule, MatCardModule, MatExpansionModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
