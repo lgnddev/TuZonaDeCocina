b33aeadbb923bec35eae9da0f73300d32c0d2b00
Jorge <jor.saezb@duocuc.cl>
2021-10-19 19:44:00 -0300

    receta 1.0

--- src/app/componentes/desayuno/desayuno.component.ts
+++ src/app/componentes/desayuno/desayuno.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-desayuno',
@@ -6,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./desayuno.component.scss'],
 })
 export class DesayunoComponent implements OnInit {
+  user: any;
 
-  constructor() { }
+  constructor(private router: Router, private activeroute: ActivatedRoute, private activatedRoute: ActivatedRoute) {
+    this.activeroute.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.user = this.router.getCurrentNavigation().extras.state.user;
+      }
+    });
+  }
+  ngOnInit() { }
 
-  ngOnInit() {}
+  sendUser() {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        user: this.user
+      }
+    }
+    this.router.navigate(['/receta'], navigationExtras);
 
+  }
 }
