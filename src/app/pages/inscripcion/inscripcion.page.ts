45560d1c141a852d153de48df371fa6883fec721
Jorge <jor.saezb@duocuc.cl>
2021-11-04 18:10:43 -0300

    c

--- src/app/pages/inscripcion/inscripcion.page.ts
+++ src/app/pages/inscripcion/inscripcion.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { BDService } from 'src/app/servicios/bd.service';
 
 @Component({
@@ -8,8 +9,9 @@ import { BDService } from 'src/app/servicios/bd.service';
   styleUrls: ['./inscripcion.page.scss'],
 })
 export class InscripcionPage implements OnInit {
+  
 
-  constructor(private router: Router, private servicioDB: BDService) { }
+  constructor(private router: Router, private servicioDB: BDService, public toastController: ToastController) { }
 
   ngOnInit() {
       this.servicioDB.dbState().subscribe((res) =>{
@@ -33,7 +35,19 @@ export class InscripcionPage implements OnInit {
 
   registrar() {
     this.servicioDB.addUsuario(this.nuevoUsuario.nombre, this.nuevoUsuario.apellido, this.nuevoUsuario.fnacimiento, this.nuevoUsuario.email, this.nuevoUsuario.contrasena, 1);
+    this.presentToast("Usuario Registrado");
   }
 
+  async presentToast(message: string, duration?: number) {
+    const toast = await this.toastController.create(
+      {
+        cssClass: 'toast-wrapper.toast-bottom',
+        message: message,
+        position: 'bottom',
+        duration: duration ? duration : 2000
+      }
+    );
+    toast.present();
+  }
 
 }
