21c268d3779cbd1eb2529cf4c8b43bf3c2b01da5
Alonso <alo.zenteno@duocuc.cl>
2021-11-27 17:28:31 -0400

    sadsa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BDService } from './servicios/bd.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private servicioDB : BDService){}
+
+  tipoUsuario: any;
   public appPages = [
     { title: 'Home', url: '/folder/Inbox', icon: 'home' },
     { title: 'Tus Recetas', url: '/tus-recetas', icon: 'book' },
@@ -15,4 +20,16 @@ export class AppComponent {
     { title: 'Perfil', url: '/perfil', icon: 'person' },
     { title: 'Cerrar Sesion', url: '/login', icon: 'log-out' },
   ];
+
+  async ngOnInit(){
+    await this.servicioDB.dbState().subscribe((res) =>{
+      if(res){
+        this.servicioDB.fetchUsuario().subscribe(item =>{
+          this.tipoUsuario = item;
+        })
+      }
+    });
+    await console.log(this.tipoUsuario + 'holaa')
+  }
+
 }
